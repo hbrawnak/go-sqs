46f3ed2547918d3136b8d92ebc5bcd803b6beed7
Habibur Rahman <hbrawnak@gmail.com>
2025-04-15 17:45:01 +0600

    producer generated to test queue

--- producer.go
+++ producer.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"os"
+)
 
-func main() {
-	fmt.Println("I'm Producer")
+func sendMessage(queueURL, message string) error {
+	region := os.Getenv("AWS_REGION")
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+	svc := sqs.New(sess)
+
+	_, err := svc.SendMessage(&sqs.SendMessageInput{
+		MessageBody: aws.String(message),
+		QueueUrl:    aws.String(queueURL),
+	})
+
+	return err
 }
